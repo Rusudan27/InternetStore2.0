f41ed7ab3761324c711cf53a8f7359e5007f93d5
Rusudan <rusudangeradze5@gmail.com>
2025-11-22 16:27:01 +0500

    I2-4.Отверстать Header и Footer.

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtLoadingIndicator />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<style lang="scss">
+body {
+  background-color: black;
+  margin: 0;
+  padding: 0;
+}
+</style>
+
